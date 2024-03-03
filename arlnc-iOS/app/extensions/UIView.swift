5426c4d0629499792645a96aeef046359aa02615
resoul <resoul.ua@icloud.com>
2024-03-03 22:59:00 +0200

    update extensions

---
+++ arlnc-iOS/app/extensions/UIView.swift
@@ -0,0 +1,14 @@
+//
+//  UIView.swift
+//  arlnc-iOS
+//
+//  Created by resoul on 03.03.2024.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ subviews: UIView...) {
+        subviews.forEach(addSubview)
+    }
+}
